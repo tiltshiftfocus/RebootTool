ed83f1a529d53f8c39776c5bbacc02580067f814
tiltshiftfocus <limzming@gmail.com>
2015-01-26 11:56:30 +0800

    Update Icon

--- app/src/main/java/com/jerry/reboottool/AboutActivity.java
+++ app/src/main/java/com/jerry/reboottool/AboutActivity.java
@@ -4,16 +4,18 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class AboutActivity extends ActionBarActivity {
 
     private Toolbar toolbar;
-	
+	private TextView versionNo;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +32,16 @@ public class AboutActivity extends ActionBarActivity {
         toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-	}
+
+        versionNo = (TextView) findViewById(R.id.version);
+        try {
+            String version = getPackageManager().getPackageInfo(getPackageName(),0).versionName;
+            versionNo.setText(version);
+        } catch (PackageManager.NameNotFoundException e) {
+
+        }
+
+    }
 	
 	public void sendEmail(View v){
 		Intent i = new Intent(Intent.ACTION_SEND);
